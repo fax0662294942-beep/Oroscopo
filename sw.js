4a47df0103a8c76cb0e77a2db0d4c7418a2d5c43
fax0662294942-beep <fax0662294942@gmail.com>
2026-06-10 16:04:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
-var CACHE="oroscopo-v1.1";
+var CACHE="oroscopo-v1.2";
 var ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));});
 self.addEventListener("activate",function(e){clients.claim();e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}));});
 self.addEventListener("fetch",function(e){
-  if(e.request.url.indexOf("googleapis.com")!==-1||e.request.url.indexOf("fonts.")!==-1||e.request.url.indexOf("unpkg.")!==-1){e.respondWith(fetch(e.request));return;}
+  var url=e.request.url;
+  if(url.indexOf("googleapis.com")!==-1||url.indexOf("gstatic.com")!==-1||url.indexOf("fonts.")!==-1||url.indexOf("unpkg.")!==-1){
+    e.respondWith(fetch(e.request)); return;
+  }
   e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));
 });
