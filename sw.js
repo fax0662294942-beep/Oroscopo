0557cb07eb61c9ca762936f7a1a225dd3746a7a6
fax0662294942-beep <fax0662294942@gmail.com>
2026-06-12 07:37:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE="oroscopo-v1.3";
+var CACHE="oroscopo-v1.4";
 var ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));});
 self.addEventListener("activate",function(e){clients.claim();e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}));});
